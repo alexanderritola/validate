08384c89111bcd4bd617521fb0c4f2f9dc1d2c05
inhies <inhies@gmail.com>
2013-10-30 12:58:14 -1000

    Finish creating simple package

--- simple/simple.go
+++ simple/simple.go
@@ -1,6 +1,8 @@
+// Package validate/simple provides common methods for validating data.
 package simple
 
 import (
+	"errors"
 	"github.com/daswasser/validate"
 	"unicode"
 	"unicode/utf8"
@@ -16,20 +18,32 @@ var (
 	//	upAlphabet = []byte("EITSANHURDMWGVLFBKOPJXCZYQ")
 )
 
+var (
+	// Invalid characters or punctuation.
+	ErrFormatting = &validate.Error{
+		Level:   validate.ErrInvalid,
+		Message: errors.New("Invalid formatting"),
+	}
+)
+
 type Printable struct {
 	data    []byte
 	message string
 }
 
+func NewPrintable(data []byte) *Printable {
+	return &Printable{data: data}
+}
+
 // Check to ensure the byte slice only contains printable UTF-8 runes
-func (m *Printable) Validate(v *validate.Validator) *validate.Error {
+func (m *Printable) Validate(v validate.Validator) *validate.Error {
 	p := m.data
 	// Borrowed from utf.Valid() with added checks for printable runes
 	for i := 0; i < len(p); {
 		if p[i] < utf8.RuneSelf {
 			// Check if this single byte run is printable
 			if !unicode.IsPrint(rune(p[i])) {
-				return nil
+				return ErrFormatting
 			}
 			i++
 		} else {
@@ -38,11 +52,11 @@ func (m *Printable) Validate(v *validate.Validator) *validate.Error {
 				// All valid runes of size 1 (those
 				// below RuneSelf) were handled above.
 				// This must be a RuneError.
-				return nil
+				return validate.ErrInvalidUTF8
 			}
 			// Check if this multi-byte rune is printable
 			if !unicode.IsPrint(r) {
-				return nil
+				return ErrFormatting
 			}
 			i += size
 		}
@@ -50,32 +64,90 @@ func (m *Printable) Validate(v *validate.Validator) *validate.Error {
 	return nil
 }
 
+// Defines a check for all lowercase unicode points. This will fail if there is
+// any punctuation or spaces.
+// TODO(inhies): Add option for allowing a space or similar characters to not
+// fail validation.
+type Lower struct {
+	data    []byte
+	message string
+}
+
+// Returns a new Lower value for the specified data.
+func NewLower(data []byte) *Lower {
+	return &Lower{data: data}
+}
+
 // Check to ensure the byte slice only contains lower case UTF-8 runes
-func IsLower(p []byte) bool {
+func (m *Lower) Validate(v validate.Validator) *validate.Error {
+	p := m.data
 	// Borrowed from utf.Valid() with added checks for lower case runes
 	for i := 0; i < len(p); {
 		if p[i] < utf8.RuneSelf {
 			// Check if this single byte run is a lower case rune
 			if !unicode.IsLower(rune(p[i])) {
-				return false
+				return ErrFormatting
 			}
 			i++
+		} else {
+			r, size := utf8.DecodeRune(p[i:])
+			if size == 1 {
+				// All valid runes of size 1 (those
+				// below RuneSelf) were handled above.
+				// This must be a RuneError.
+				return validate.ErrInvalidUTF8
+			}
+
+			// Check if this multi-byte rune is printable
+			if !unicode.IsLower(r) {
+				return ErrFormatting
+			}
+			i += size
 		}
 	}
-	return true
+	return nil
 }
 
-// Check to ensure the byte slice only contains upper case UTF-8 runes
-func IsUpper(p []byte) bool {
-	// Borrowed from utf.Valid() with added checks for upper case runes
+// Defines a check for all uppercase  unicode points. This will fail if there is
+// any punctuation or spaces.
+// TODO(inhies): Add option for allowing a space or similar characters to not
+// fail validation.
+type Upper struct {
+	data    []byte
+	message string
+}
+
+// Returns a new Upper  value for the specified data.
+func NewUpper(data []byte) *Upper {
+	return &Upper{data: data}
+}
+
+// Check to ensure the byte slice only contains lower case UTF-8 runes
+func (m *Upper) Validate(v validate.Validator) *validate.Error {
+	p := m.data
+	// Borrowed from utf.Valid() with added checks for lower case runes
 	for i := 0; i < len(p); {
 		if p[i] < utf8.RuneSelf {
-			// Check if this single byte run is a upper case rune
+			// Check if this single byte run is a lower case rune
 			if !unicode.IsUpper(rune(p[i])) {
-				return false
+				return ErrFormatting
 			}
 			i++
+		} else {
+			r, size := utf8.DecodeRune(p[i:])
+			if size == 1 {
+				// All valid runes of size 1 (those
+				// below RuneSelf) were handled above.
+				// This must be a RuneError.
+				return validate.ErrInvalidUTF8
+			}
+
+			// Check if this multi-byte rune is printable
+			if !unicode.IsUpper(r) {
+				return ErrFormatting
+			}
+			i += size
 		}
 	}
-	return true
+	return nil
 }
--- simple/simple_test.go
+++ simple/simple_test.go
@@ -1,59 +1,115 @@
 package simple
 
 import (
+	"github.com/daswasser/validate"
 	"testing"
 )
 
-func Test_IsLower_1(t *testing.T) {
-	s := "goiscool"
-	if IsLower([]byte(s)) != true {
-		t.Error("Failed")
-	} else {
-		t.Log("Passed")
+var isPrintTests = []struct {
+	Data  string
+	Valid bool
+}{
+	{"go is cool!", true},
+	{string('\u0000'), false},
+	{"http://مثال.إختبار", true},
+}
+
+func Test_IsPrint(t *testing.T) {
+	v := validate.NewValidator()
+	for i, d := range isPrintTests {
+		data := NewPrintable([]byte(d.Data))
+		err := v.Validate(data)
+		if (err != nil && d.Valid) || (err == nil && !d.Valid) {
+			t.Errorf(
+				"%d. IsLower(\"%s\") Error=%v, Valid=%v. Error value: %s\n",
+				i, d.Data, err != nil, d.Valid, err)
+		}
 	}
 }
 
-func Test_IsLower_2(t *testing.T) {
-	s := "abcdefghijklmnopqrstuvwxyz"
-	if IsLower([]byte(s)) != true {
-		t.Error("Failed")
-	} else {
-		t.Log("Passed")
+func Benchmark_IsPrint(b *testing.B) {
+	b.StopTimer()
+	v := validate.NewValidator()
+	for _, d := range isPrintTests {
+		data := NewPrintable([]byte(d.Data))
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			v.Validate(data)
+		}
+		b.StopTimer()
 	}
 }
 
-func Test_IsPrint_1(t *testing.T) {
-	s := "goiscool"
-	if IsPrint([]byte(s)) != true {
-		t.Error("Failed")
-	} else {
-		t.Log("Passed")
+var isLowerTests = []struct {
+	Data  string
+	Valid bool
+}{
+	{"abcdefghijklmnopqrstuv", true},
+	{"CRUISECONTROLFORCOOL", false},
+	{"spaces are bad", false},
+	{"so is punctuation!", false},
+	{"thisisokthough", true},
+}
+
+func Test_IsLower(t *testing.T) {
+	v := validate.NewValidator()
+	for i, d := range isLowerTests {
+		data := NewLower([]byte(d.Data))
+		err := v.Validate(data)
+		if (err != nil && d.Valid) || (err == nil && !d.Valid) {
+			t.Errorf(
+				"%d. IsLower(\"%s\") Error=%v, Valid=%v. Error value: %s\n",
+				i, d.Data, err != nil, d.Valid, err)
+		}
+	}
+
+}
+
+func Benchmark_IsLower(b *testing.B) {
+	b.StopTimer()
+	v := validate.NewValidator()
+	for _, d := range isLowerTests {
+		data := NewLower([]byte(d.Data))
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			v.Validate(data)
+		}
+		b.StopTimer()
 	}
 }
 
-func Test_IsPrint_2(t *testing.T) {
-	s := "abcdefghijklmnopqrstuvwxyz"
-	if IsPrint([]byte(s)) != true {
-		t.Error("Failed")
-	} else {
-		t.Log("Passed")
+var isUpperTests = []struct {
+	Data  string
+	Valid bool
+}{
+	{"abcdefghijklmnopqrstuv", false},
+	{"CRUISECONTROLFORCOOL", true},
+	{"spaces are bad", false},
+	{"so is punctuation!", false},
+	{"THISISOKTHOUGH", true},
+}
+
+func Test_IsUpper(t *testing.T) {
+	v := validate.NewValidator()
+	for i, d := range isUpperTests {
+		data := NewUpper([]byte(d.Data))
+		err := v.Validate(data)
+		if (err != nil && d.Valid) || (err == nil && !d.Valid) {
+			t.Errorf(
+				"%d. IsUpper(\"%s\") Error=%v, Valid=%v. Error value: %s\n",
+				i, d.Data, err != nil, d.Valid, err)
+		}
 	}
 }
 
-func Test_IsUpper_1(t *testing.T) {
-	s := "GOISCOOL"
-	if IsUpper([]byte(s)) != true {
-		t.Error("Failed")
-	} else {
-		t.Log("Passed")
-	}
-}
-
-func Test_IsUpper_2(t *testing.T) {
-	s := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	if IsUpper([]byte(s)) != true {
-		t.Error("Failed")
-	} else {
-		t.Log("Passed")
+func Benchmark_IsUpper(b *testing.B) {
+	b.StopTimer()
+	v := validate.NewValidator()
+	for _, d := range isUpperTests {
+		data := NewUpper([]byte(d.Data))
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			v.Validate(data)
+		}
 	}
 }
